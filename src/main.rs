68e57095c4724ca5a05e682d20a85aee045d7025
WalkerRout <89273280+WalkerRout@users.noreply.github.com>
2022-12-06 13:59:52 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 macro_rules! check_expect {
-  ($test_name:ident : $expr1:expr => $expr2:expr) => {
+  ($test_name:ident -> $expr1:expr => $expr2:expr) => {
     #[test]
     fn $test_name() {
       assert_eq!($expr1, $expr2);
@@ -8,14 +8,14 @@ macro_rules! check_expect {
 }
 
 // for longer unit tests
-check_expect!(add_0_1 : {
+check_expect!(add_0_1 -> {
   let a = 0;
   let b = 1;
   add(a, b)
 } => 1);
-check_expect!(add_2_3 : add(2, 3) => 5);
-check_expect!(add_4_5 : add(4, 5) => 9);
-check_expect!(add_0_0 : add(0, 0) => 0);
+check_expect!(add_2_3 -> add(2, 3) => 5);
+check_expect!(add_4_5 -> add(4, 5) => 9);
+check_expect!(add_0_0 -> add(0, 0) => 0);
 pub fn add(a: i32, b: i32) -> i32 {
   a + b
 }
